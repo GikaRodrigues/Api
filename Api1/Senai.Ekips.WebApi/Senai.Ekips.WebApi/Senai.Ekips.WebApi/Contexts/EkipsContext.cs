1a908d5797054b6f4f6b7abd45995859a2df64e8
GikaRodrigues <48352579+GikaRodrigues@users.noreply.github.com>
2019-08-30 11:17:46 -0300

    up

---
+++ Api1/Senai.Ekips.WebApi/Senai.Ekips.WebApi/Senai.Ekips.WebApi/Contexts/EkipsContext.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Senai.Ekips.WebApi.Domains
+{
+    public partial class EkipsContext : DbContext
+    {
+        public EkipsContext()
+        {
+        }
+
+        public EkipsContext(DbContextOptions<EkipsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cargos> Cargos { get; set; }
+        public virtual DbSet<Departamentos> Departamentos { get; set; }
+        public virtual DbSet<Funcionarios> Funcionarios { get; set; }
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=M_Ekips;User Id=sa;Pwd=132");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cargos>(entity =>
+            {
+                entity.HasKey(e => e.IdCargo);
+
+                entity.Property(e => e.Cargo)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Statuss)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Departamentos>(entity =>
+            {
+                entity.HasKey(e => e.IdDepartamento);
+
+                entity.HasIndex(e => e.Departamento)
+                    .HasName("UQ__Departam__71BEBBD7D449044C")
+                    .IsUnique();
+
+                entity.Property(e => e.Departamento)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Funcionarios>(entity =>
+            {
+                entity.HasKey(e => e.IdFuncionario);
+
+                entity.HasIndex(e => e.Cpf)
+                    .HasName("UQ__Funciona__C1F897317A71779F")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.Funcionario)
+                    .HasName("UQ__Funciona__8040ADB360A840D0")
+                    .IsUnique();
+
+                entity.Property(e => e.Cpf)
+                    .IsRequired()
+                    .HasColumnName("CPF")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DataNascimento).HasColumnType("date");
+
+                entity.Property(e => e.Funcionario)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdCargoNavigation)
+                    .WithMany(p => p.Funcionarios)
+                    .HasForeignKey(d => d.IdCargo)
+                    .HasConstraintName("FK__Funcionar__IdCar__5812160E");
+
+                entity.HasOne(d => d.IdDepartamentoNavigation)
+                    .WithMany(p => p.Funcionarios)
+                    .HasForeignKey(d => d.IdDepartamento)
+                    .HasConstraintName("FK__Funcionar__IdDep__571DF1D5");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Funcionarios)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .HasConstraintName("FK__Funcionar__IdUsu__59063A47");
+            });
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("UQ__Usuarios__A9D105342E63EEC7")
+                    .IsUnique();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+            });
+        }
+    }
+}
